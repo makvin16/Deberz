0d7740243db448fe1b8c30e2d5fdc448c33ab23a
Евгений Москаленко < >
2017-01-08 00:17:53 +0300

    тестирую

---
+++ android/src/com/mydeberz/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mydeberz;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mydeberz.MyDeberz;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyDeberz(), config);
+	}
+}
